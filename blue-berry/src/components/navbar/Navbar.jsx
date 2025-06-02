3a563333aed359837f411a410a1e5e7f235a7642
Dinesh <youremail@example.com>
2025-06-02 18:23:43 +0530

    Second commit

--- blue-berry/src/components/navbar/Navbar.jsx
+++ blue-berry/src/components/navbar/Navbar.jsx
@@ -1,17 +1,52 @@
 import './Navbar.css';
 import {Link} from 'react-router-dom';  
+import { RiContactsLine } from "react-icons/ri";
+import { FaRegStar } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+
 
 const Navbar = () => {
   return (
     <div className='nav-container'>
-        <h1><Link to={'/'}>blue berry</Link></h1>
+            <div className="navbar-top">
+        <div className="navbar-heading">
+            <img src="https://blueberry-react-next.maraviyainfotech.com/assets/img/logo/logo.png" alt="img" />
+        </div>
+        <div className="navbar-input">
+            <button>vegetables</button>
+            <input type="text" placeholder='search products....'/>
+        </div>
+
+    <div className="navbar-icons">
+        <div className="navbar-1">
+            <h4><RiContactsLine /><span>login</span></h4>
+        </div>
+
+        <div className="navbar-1">
+            <h4><FaRegStar /> <span>Wishlist</span></h4>
+        </div>
+
+        <div className="navbar-1">
+            <h4><MdOutlineShoppingCart /> <span>cart</span></h4>
+        </div>
+    </div>
+         </div>
+
+
+
+
+
+
+
+        <div className="navbar-last">
         <ul>
-            <li><Link to={'/'}>Home</Link></li>
-            <li><Link to={'/categories'}>categories</Link></li>
-            <li><Link to={"/Products"}>Products</Link></li>
-            <li><Link to={"/Page"}>Page</Link></li>
-            <li><Link to={"/Blog"}>Blog</Link></li>
+            <li><Link className='nav-link' to={'/'}>Home</Link></li>
+            <li><Link className='nav-link' to={'/categories'}>categories</Link></li>
+            <li><Link className='nav-link' to={"/Products"}>Products</Link></li>
+            <li><Link className='nav-link' to={"/Page"}>Page</Link></li>
+            <li><Link className='nav-link' to={"/Blog"}>Blog</Link></li>
         </ul>
+        </div>
     </div>    
   )
 }
